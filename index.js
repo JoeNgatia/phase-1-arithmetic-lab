62a4c9a6cb5d6849a494b61d879b4fece3e1b302
Joe <ngatiajoe2@gmail.com>
2024-10-31 20:53:52 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-const multiply=(num1, num2) => {
-    let result = num1 * num2 +6
-    return result
+// Challenge 1: Create a variable called multiply that multiplies num1 and num2 to get 62
+let num1 = 31; // Example value
+let num2 = 2;  // Example value
+const multiply = num1 * num2; // This will result in 62
 
-}
+// Challenge 2: Create a variable called random that generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
 
-let num1=8;
-let num2=7;
+// Challenge 3: Create a variable called mod that calculates the remainder of num3 divided by num4 to get 4
+let num3 = 24; // Example value
+let num4 = 10; // Example value
+const mod = num3 % num4; // This will result in 4
 
+// Challenge 4: Create a variable called max that finds the highest number in a set to return 20
+const max = Math.max(10, 15, 20, 5); // This will return 20
 
-console.log(multiply(num1,num2))
-
-const random=() =>{
-    let randomNum=Math.floor(Math.random()*100) +1
-    return randomNum
-}
-
-console.log(random())
-
-const mod=(num3,num4) =>{
-    return num3/num4;
-}
-
-let num3=16
-let num4=4
-
-console.log(mod(num3,num4))
-
-const max = (x ,y1 ,w ,t, y2 ,u) => {
-
-    let Num=Math.max(x ,y1, w, t, y2, u);
-    return Num
-}
-
-let x=10
-let y1=20
-let w=5
-let t=15
-let y2=15
-let u=19
-
-console.log(max(x,y1,w,t,y2,u))
+// Logging the results to verify correctness
+console.log("Multiply:", multiply); // Should output: Multiply: 62
+console.log("Random:", random);      // Should output: Random: (some integer > 0)
+console.log("Mod:", mod);            // Should output: Mod: 4
+console.log("Max:", max);            // Should output: Max: 20
